d1a3c046e2564c6bb42b9f0651d633105046fb1f
Sachin Athukorala <sachin2262716@gmail.com>
2022-03-31 16:50:16 +0530

    added validation to review form

--- src/app/pages/review/review.component.ts
+++ src/app/pages/review/review.component.ts
@@ -11,14 +11,32 @@ export class ReviewComponent implements OnInit {
   id: string = '';
   form: FormGroup;
   currentRating: number = 3;
-  inputRating: number = 0;
+  isInputRateChanged: boolean = false;
+  isLoading: boolean = false;
 
   constructor(private router: Router, private route: ActivatedRoute, private fb: FormBuilder) {
     this.form = this.fb.group({
+      rating: [0, [Validators.required]],
       name: ['', Validators.required],
       gender: ['', [Validators.required]],
       review: ['', [Validators.required]]
     });
+  }
+
+  get rating() {
+    return this.form.get('rating');
+  }
+
+  get name() {
+    return this.form.get('name');
+  }
+
+  get gender() {
+    return this.form.get('gender');
+  }
+
+  get review() {
+    return this.form.get('review');
   }
 
   ngOnInit(): void {
@@ -28,8 +46,59 @@ export class ReviewComponent implements OnInit {
 
   }
 
-  submit() {
-    console.log("submit")
+  /*
+    return error classes for input fields if errors exists
+  */
+  inputClasses(fieldName: string) {
+    let isError: boolean = false;
+
+    switch(fieldName) {
+      case 'name':
+        if ( (this.name?.invalid && (this.name?.dirty || this.name?.touched)) && this.name?.errors?.['required']) {
+          isError = true;
+        }
+        break;
+      case 'gender':
+        if ( (this.gender?.invalid && (this.gender?.dirty || this.gender?.touched)) && this.gender?.errors?.['required']) {
+          isError = true;
+        }
+        break;
+      case 'review':
+        if ( (this.review?.invalid && (this.review?.dirty || this.review?.touched)) && this.review?.errors?.['required']) {
+          isError = true;
+        }
+        break;
+      default:
+        return
+    }
+
+    return { 'input-error': isError };
+  }
+
+  /*
+    submit review form
+  */
+  onSubmit() {
+    if (this.form.status === 'INVALID') {
+      return;
+    }
+    this.isLoading = true;
+
+    const newReview = {
+      id: this.id,
+      rating: this.rating?.value,
+      name: this.name?.value,
+      gender: this.gender?.value,
+      review: this.review?.value
+    }
+
+    setTimeout(() => {
+      this.isLoading = false;
+      console.log(newReview);
+      this.form.reset();
+    }, 5000);
+
+
   }
 
 }
